f26bbf12c8849dd43f21f56c6fe9e5b7e7680581
id2650-ensai <siham.atmani@eleve.ensai.fr>
2025-09-05 10:44:11 +0000

    changement attaques

--- src/business_object/attaques/fix_damage_attack.py
+++ src/business_object/attaques/fix_damage_attack.py
@@ -3,8 +3,7 @@ from business_object.attaques.abstract_attack import AbstractAttack
 
 class FixedDamageAttack(AbstractAttack):
     def __init__(self, power: int, name: str, description: str):
-        super().__init__(self, power: int, name: str, description: str)
-
+        super().__init__(power, name, description)
 
     def compute_damage(self, attacker, defender) -> int:
         return self._power
